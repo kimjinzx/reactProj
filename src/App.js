8835bdca78020d12c621eb1f478300de1873ebcd
jin <jinhyun7675@gmail.com>
2023-08-16 23:16:24 +0900

    ERD제작 및 useState연습

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+// function App() {
+
+//   let num = 1;
+//   function numUp(){
+//     num += 1;
+//     console.log(num);
+//   }
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <div>{num}</div>
+//         <button onClick={() => {numUp()}}>버튼</button>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// function App() {
+//   let [num, setNum] = useState(1);
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <div>{num}</div>
+//         <button onClick={() => {setNum(num + 1)}}>버튼</button>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 function App() {
+  let [num, setNum] = useState(1);
+
+  setTimeout(() => {setNum(num += 1)},1000);
+    
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>{num}</div>
       </header>
     </div>
   );
 }
 
 export default App;
+
